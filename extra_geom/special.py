5351ad32517cbc63efe389a2f9cd073552fedb45
tmichela <thomas.michelat@gmail.com>
2022-06-13 16:37:33 +0200

    rename function, merge arguments for edge mask, input SourceData object instead of run/sourcename, import the function from the base module

--- extra_geom/special.py
+++ extra_geom/special.py
@@ -1,23 +1,28 @@
+from warnings import warn
+
 import numpy as np
 
 from .detectors import JUNGFRAUGeometry as JF
 
 
-def jungfrau_data(dc, name, use_mask=True, mask_edges=True, pix_edge=1,
-                  trains=np.s_[:]):
-    """Get Jungfrau data for the given detector in the data collection
+def jungfrau_module_data(data, use_mask=True, edge_pixels=1, trains=np.s_[:]):
+    """Get data for the given JUNGFRAU detector
 
     The returned data has inter ASIC gaps added and masked applied if requested
 
-    dc: extra_data.DataCollection
-        an extra_data data collection
-    name: str
-        name of the detector
+    ::
+
+        from extra_data import open_run
+
+        run = open_run(1234, 1)
+        jungfrau = run['HED_IA1_JF500K1/DET/JNGFR01:daqOutput']
+        data = jungfrau_module_data(jungfrau)
+
+    data: extra_data.SourceData
+        A SourceData object for a JUNGFRAU detector
     use_mask: bool
         if True, use the mask present in the data (if in use with CORR data)
-    mask_edges: bool
-        if True, mask the edges of the detector
-    pix_edge: int
+    edge_pixels: int
         number of pixels to mask at the edges of detector ASICS
     trains: slice
         Select a subset of trains from this data collection.
@@ -28,17 +33,19 @@ def jungfrau_data(dc, name, use_mask=True, mask_edges=True, pix_edge=1,
                 by_id[142844490 : 142844495]
                 by_id[[142844490, 142844493, 142844494]]
     """
-    jf = dc[name]
-    jf = jf.select_trains(trains)
+    jf = data.select_trains(trains)
     data = jf['data.adc'].ndarray()
 
-    if use_mask and 'data.mask' in jf.keys():
-        mask = jf['data.mask'].ndarray().astype(np.bool_)
-        data = data * ~mask
+    if use_mask:
+        if 'data.mask' in jf.keys():
+            mask = jf['data.mask'].ndarray().astype(np.bool_)
+            data = data * ~mask
+        else:
+            warn("No mask present in data")
 
-    if mask_edges:
+    if edge_pixels > 0:
         asic = np.zeros((JF.frag_ss_pixels, JF.frag_fs_pixels), dtype=np.bool_)
-        asic[pix_edge:-pix_edge, pix_edge:-pix_edge] = True
+        asic[edge_pixels:-edge_pixels, edge_pixels:-edge_pixels] = True
         edge_mask = np.tile(
             asic, [JF.expected_data_shape[-2] // JF.frag_ss_pixels,
                    JF.expected_data_shape[-1] // JF.frag_fs_pixels])
