208cd205c6fde9cef2610da1bb76dcc87fa2fa08
Egor Sobolev <egor.sobolev@xfel.eu>
2024-03-26 09:39:06 +0100

    Add more motor tests

--- extra_geom/tests/test_motors.py
+++ extra_geom/tests/test_motors.py
@@ -2,15 +2,25 @@ import numpy as np
 import pytest
 from extra_geom import AGIPD_1MGeometry
 from extra_geom.crystfel_fmt import motors_to_geom
-from extra_geom.motors import AGIPD_1MMotors, read_motors_from_geom
+from extra_geom.motors import (AGIPD_1MMotors, BaseMotorTracker,
+                               read_motors_from_geom)
 
-motor_text_reference = """\
-;XGEOM MOTORS=4,2
-;XGEOM MOTOR_Q1=1,-2
-;XGEOM MOTOR_Q2=1,-2
-;XGEOM MOTOR_Q3=1,-2
-;XGEOM MOTOR_Q4=1,-2
+motor_text_template = """\
+;XGEOM MOTORS={num_groups},{num_motors}
+;XGEOM MOTOR_Q1={q1m1},{q1m2}
+;XGEOM MOTOR_Q2={q2m1},{q2m2}
+;XGEOM MOTOR_Q3={q3m1},{q3m2}
+;XGEOM MOTOR_Q4={q4m1},{q4m2}
 """
+
+
+class InvalidMotorTracker(BaseMotorTracker):
+    groups = [
+        np.s_[0:4], np.s_[4:8],
+    ]
+    default_motor_axes = np.array([
+        [[-1, 0], [0, -1]],  # Q1
+    ])
 
 
 def test_move_geom_by():
@@ -23,6 +33,9 @@ def test_move_geom_by():
     geom2 = tracker.move_geom_by(motor_pos)
 
     np.testing.assert_allclose(geom2.quad_positions(), quad_pos_new)
+
+    with pytest.raises(ValueError):
+        tracker.move_geom_by([[1, -2]] * 3)
 
 
 def test_geom_at():
@@ -50,12 +63,68 @@ def test_geom_at():
 
     np.testing.assert_allclose(geom2.quad_positions(), quad_pos_new)
 
+    with pytest.raises(ValueError):
+        tracker.geom_at([[1, -2]] * 3)
+
 
 def test_read_write_motors():
     motor_pos = [[1, -2], [1, -2], [1, -2], [1, -2]]
+    motor_param = {
+        f"q{i // 2 + 1}m{i % 2 + 1}": motor_pos[i // 2][i % 2]
+        for i in range(8)
+    }
+    motor_param.update({
+        "num_groups": len(motor_pos),
+        "num_motors": len(motor_pos[0]),
+    })
+    motor_text_reference = motor_text_template.format(**motor_param)
 
     motor_text = motors_to_geom(motor_pos)
     assert motor_text_reference == motor_text
 
     motor_pos_read = read_motors_from_geom(motor_text)
     np.testing.assert_allclose(motor_pos, motor_pos_read)
+
+    motor_invalid_param = motor_param.copy()
+    motor_invalid_param["num_groups"] = 'a'
+    motor_text = motor_text_template.format(**motor_invalid_param)
+    with pytest.raises(ValueError):
+        read_motors_from_geom(motor_text)
+
+    motor_invalid_param = motor_param.copy()
+    motor_invalid_param["q2m2"] = 'a'
+    motor_text = motor_text_template.format(**motor_invalid_param)
+    with pytest.raises(ValueError):
+        read_motors_from_geom(motor_text)
+
+    motor_invalid_param = motor_param.copy()
+    motor_invalid_param["q2m2"] = "0,0"
+    motor_text = motor_text_template.format(**motor_invalid_param)
+    with pytest.raises(ValueError):
+        read_motors_from_geom(motor_text)
+
+    motor_text = motor_text_template.format(**motor_param)
+    motor_text = '\n'.join(motor_text.split('\n')[:-2] + [""])
+    with pytest.raises(ValueError):
+        read_motors_from_geom(motor_text)
+
+
+def test_other_methods():
+    quad_pos = [(-525, 625), (-550, -10), (520, -160), (542.5, 475)]
+    quad_pos_new = [(-520, 615), (-545., 0.), (515., -150.), (537.5, 465.)]
+    motor_pos = [[1, -2], [1, -2], [1, -2], [1, -2]]
+
+    geom = AGIPD_1MGeometry.from_quad_positions(quad_pos)
+    with pytest.raises(ValueError):
+        InvalidMotorTracker(geom)
+
+    tracker = AGIPD_1MMotors(geom)
+    with pytest.raises(ValueError):
+        tracker.set_reference_positions([[1, -2]] * 3)
+
+    with pytest.raises(ValueError):
+        tracker.set_motor_axes(InvalidMotorTracker.default_motor_axes)
+
+    tracker.set_motor_axes(AGIPD_1MMotors.default_motor_axes * -1)
+    geom2 = tracker.move_geom_by(motor_pos)
+    np.testing.assert_allclose(geom2.quad_positions(), quad_pos_new)
